d00ac705f292e02ac55e00e20827e99e5da60bb2
Iswb04 <isabellaramos654@gmail.com>
2025-11-04 16:11:10 -0300

    041125

--- py_sql.py
+++ py_sql.py
@@ -1,1 +1,28 @@
-...
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+    'name TEXT, '
+    'weight REAL'
+    ')'
+)
+connection.commit()
+
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+    'VALUES (NULL, "Enfermeiro Elson", 9.9)'
+)
+connection.commit()
+
+cursor.close()
+connection.close()
